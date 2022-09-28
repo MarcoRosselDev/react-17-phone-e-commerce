37c8ccae2ecc68d6abcecb318d0d197f98df0b29
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-28 17:20:37 -0300

    adding state products

--- src/context.js
+++ src/context.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
+import { storeProducts, detailProduct } from "./data";
 
 const ProductContext = React.createContext();
 //provider
 //consumer
 
 export default class ProductProvider extends Component {
+  state = {
+    paducts: storeProducts,
+    detailProduct: detailProduct,
+  };
+  handleDetail = () => {
+    console.log("hola desde detail");
+  };
+  addToCart = () => {
+    console.log("hola desde add to cart");
+  };
   render() {
     return (
-      <ProductContext.Provider value="hola desde context <3">
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
