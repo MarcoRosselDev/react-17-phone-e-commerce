a3d0d084d8497767446c3a2c77009d711a4d910f
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-27 23:51:22 -0300

    style componets solving some bugs

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import logo from "../logo.svg";
 //import styled from "styled-components";
 import { ButtonContainer } from "./Button";
@@ -7,7 +8,7 @@ import { ButtonContainer } from "./Button";
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <Nav className="navbar navbar-expand-sm  navbar-dark px-sm-5">
         {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
   https://www.iconfinder.com/Makoto_msk */}
@@ -29,7 +30,16 @@ Creative Commons (Attribution 3.0 Unported);
             my cart
           </ButtonContainer>
         </Link>
-      </nav>
+      </Nav>
     );
   }
 }
+
+const Nav = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`;
