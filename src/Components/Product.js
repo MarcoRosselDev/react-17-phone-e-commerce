a3e1814e6c2073ab9069f2edeef55214c0b10912
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-29 11:53:09 -0300

    button inCart

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -16,7 +16,21 @@ export default class Product extends Component {
             <Link to="/details">
               <img src={img} alt="product" className="card-img-top" />
             </Link>
-            <button className="cart-btn"></button>
+            <button
+              className="cart-btn"
+              disabled={inCart ? true : false}
+              onClick={() => {
+                console.log("added to the cart");
+              }}
+            >
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                  in inCart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
+            </button>
           </div>
         </div>
       </ProductWrapper>
