f57ec3b17d88a9060b64d66caeb5b8fdbd239801
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-29 12:22:42 -0300

    card footer styles transsition 2

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -64,15 +64,29 @@ const ProductWrapper = styled.div`
     .card-footer {
       background: rgba(247, 247, 247);
     }
-    .img-container {
-      position: relative;
-      overflow: hidden;
-    }
-    .card-img-top {
-      transition: all 1s linear;
-    }
-    .img-container:hover .card-img-top {
-      transform: scale(1.2);
-    }
+  }
+  .img-container {
+    position: relative;
+    overflow: hidden;
+  }
+  .card-img-top {
+    transition: all 1s linear;
+  }
+  .img-container:hover .card-img-top {
+    transform: scale(1.2);
+  }
+  .cart-btn {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 0.2rem 0.4rem;
+    background: var(--lightBlue);
+    border: none;
+    color: var(--mainWhite);
+    font-size: 1.4rem;
+    border-radius: 0.5rem 0 0 0;
+    transform: translate(100%, 100%);
+  }
+  .img-container:hover .cart-btn {
   }
 `;
