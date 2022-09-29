84f72d591065a9be932c72633ea392417a432cba
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-29 01:05:40 -0300

    add import in product.js

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 
 export default class Product extends Component {
   render() {
