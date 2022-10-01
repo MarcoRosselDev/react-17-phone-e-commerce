7ab851e400eef0378d705d48124a1b1e577983e9
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-01 20:33:10 -0300

    cart items product name

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -13,6 +13,11 @@ export default function CartItem({ item, value }) {
           className="img-fluid"
         />
       </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <span className="d-lg-none">product : </span>
+        {title}
+      </div>
+      <div className="col-10 mx-auto col-lg-2"></div>
     </div>
   );
 }
