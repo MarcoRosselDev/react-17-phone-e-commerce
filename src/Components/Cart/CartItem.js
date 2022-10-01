9425f2521fc1ba899868f4626c5daf15a96ec8c2
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-01 20:30:17 -0300

    cart items img

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function CartItem() {
-  return <div>this is a cart item</div>;
+export default function CartItem({ item, value }) {
+  const { id, title, img, price, total, count } = item;
+  const { increment, decrement, removeItem } = value;
+  return (
+    <div className="row my-1 text-capitalize text-center">
+      <div className="col-10 mx-auto col-lg-2">
+        <img
+          src={img}
+          alt="product"
+          style={{ width: "5rem", height: "5rem" }}
+          className="img-fluid"
+        />
+      </div>
+    </div>
+  );
 }
